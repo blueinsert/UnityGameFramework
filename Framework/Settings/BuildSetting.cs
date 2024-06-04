77e619a4d2a489e15ffc759e652ab8ee156f3efe
blueinsert <1401597377@qq.com>
2024-06-04 21:30:28 +0800

    new

--- Framework/Settings/BuildSetting.cs
+++ Framework/Settings/BuildSetting.cs
@@ -24,12 +24,12 @@ namespace bluebean.UGFramework
         [SerializeField]
         public string WinExeDir = "../Build/Win/";
         [SerializeField]
-        public string WinExeName = "YaYa内镜模拟.exe";
+        public string WinExeName = "demo.exe";
         [Header("Apk打包目录")]
         [SerializeField]
         public string AndroidApkDir = "../Build/Apk/";
         [SerializeField]
-        public string AndroidApkName = "YaYa内镜模拟.apk";
+        public string AndroidApkName = "demo.apk";
 
 #if UNITY_EDITOR
         [UnityEditor.MenuItem("Framework/Setting/CreateBuildSetting")]
