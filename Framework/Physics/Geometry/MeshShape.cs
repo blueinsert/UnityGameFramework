af06fabd8e3d378e90285b41c6f7a5a11e67eeb5
blueinsert <1401597377@qq.com>
2024-08-05 22:24:03 +0800

    new

--- Framework/Physics/Geometry/MeshShape.cs
+++ Framework/Physics/Geometry/MeshShape.cs
@@ -1,10 +1,8 @@
 using bluebean.UGFramework.DataStruct;
-using bluebean.UGFramework.DataStruct.Native;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace bluebean.UGFramework.Geometry
 {
