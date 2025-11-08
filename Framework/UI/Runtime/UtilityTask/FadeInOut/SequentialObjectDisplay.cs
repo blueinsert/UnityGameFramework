435095eb36fcb5e9d0cafcb1af3cf3c9f7d876ae
blueinsert <1401597377@qq.com>
2025-11-08 14:03:49 +0800

    new

---
+++ Framework/UI/Runtime/UtilityTask/FadeInOut/SequentialObjectDisplay.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace bluebean.UGFramework.UI
+{
+    public class SequentialObjectDisplay : MonoBehaviour
+    {
+        [Header("需要显示的物体")]
+        public GameObject[] objectsToDisplay = new GameObject[3];
+
+        [Header("显示时间间隔")]
+        public float displayInterval = 1f;
+
+        private int currentIndex = 0;
+        private Coroutine displayCoroutine;
+
+        void Start()
+        {
+            // 初始时隐藏所有物体
+            HideAllObjects();
+
+            // 开始显示循环
+            displayCoroutine = StartCoroutine(DisplaySequence());
+        }
+
+        IEnumerator DisplaySequence()
+        {
+            //Debug.Log("DisplaySequence start");
+            while (true)
+            {
+                // 隐藏所有物体
+                HideAllObjects();
+
+                // 显示当前物体
+                if (objectsToDisplay[currentIndex] != null)
+                {
+                    objectsToDisplay[currentIndex].SetActive(true);
+                }
+
+                // 等待指定时间
+                yield return new UnityEngine.WaitForSeconds(displayInterval);
+
+                // 移动到下一个物体
+                currentIndex = (currentIndex + 1) % objectsToDisplay.Length;
+                //Debug.Log($"{DateTime.Now.ToString("HHmmss:ff")} DisplaySequence currentIndex:{currentIndex}");
+
+            }
+        }
+
+        void HideAllObjects()
+        {
+            foreach (GameObject obj in objectsToDisplay)
+            {
+                if (obj != null)
+                {
+                    obj.SetActive(false);
+                }
+            }
+        }
+
+        // 提供公共方法控制显示循环
+        public void StartDisplay()
+        {
+            if (displayCoroutine == null)
+            {
+                displayCoroutine = StartCoroutine(DisplaySequence());
+            }
+        }
+
+        public void StopDisplay()
+        {
+            if (displayCoroutine != null)
+            {
+                StopCoroutine(displayCoroutine);
+                displayCoroutine = null;
+            }
+            HideAllObjects();
+        }
+
+        void OnDestroy()
+        {
+            // 确保在对象销毁时停止协程
+            StopDisplay();
+        }
+    }
+}
